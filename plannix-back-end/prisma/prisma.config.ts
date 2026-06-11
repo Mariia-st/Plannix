5b70a86658834cb43b1b51f381625d103d923eec
Marria <maskastarik@gmail.com>
2026-06-11 19:52:35 +0200

    refactor: actualizar prisma a v5.22.0 y limpiar configuración
    - Se actualizaron las dependencias de prisma y @prisma/client
    - Se corrigieron los archivos afectados por el cambio de versión
    - Se eliminó el archivo prisma.config por ya no ser necesario
    -- Se creó el fichero db.js para centralizar la instancia de Prisma Client

--- plannix-back-end/prisma/prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'prisma/config';
-//Archivo de configuración con prisma 
-//conecta a nuestra bbdd usando variable de entorno 
-export default defineConfig({
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
-});
