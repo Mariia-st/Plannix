b3dbddb4f818422d62452725e983d35d3f54f8e7
Marria <maskastarik@gmail.com>
2026-06-12 19:16:34 +0200

    feat: Descargar el react, crear axios
    -axios con interceptor y baseurl

---
+++ plannix-front-end/src/api/axios.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+//configuracion base de mi api
+const api=axios.create({
+    baseURL: 'http://localhost:3000', //mi back
+    headers:{
+        'Content-Type':'application/json'
+    },
+});
+
+//se ejecuta antes de cada pet
+api.interceptors.request.use((config)=>{
+    const token= localStorage.getItem('token');
+    if(token){
+        // si existe añadimos header
+        config.headers.Authorization=`Bearer ${token}`
+    }
+
+    return config
+},(error)=>{
+    return Promise.reject(error)
+})
+
+export default api
