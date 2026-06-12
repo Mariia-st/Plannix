1d5dc9d35d6fabc39e67fd79b4d499f59d278d10
Marria <maskastarik@gmail.com>
2026-06-12 18:20:05 +0200

    feat: Añadir métodos al taskController
    -Creados métodos de eliminar, modificar, y sacar solo una tarea
    -Añadido validación de titulo como es obligatorio

--- plannix-back-end/src/taskController.js
+++ plannix-back-end/src/taskController.js
@@ -20,6 +20,10 @@ const createTasks= async (req,res)=>{
     const userId= req.user.userId
     const {title,description,status,priority, deadline}=req.body
 
+    if(!title){
+        req.status(400).json({error:"El titulo es obligatorio"})
+    }
+
     try{
         const newTask = await prisma.task.create({
             data:{
@@ -34,6 +38,76 @@ const createTasks= async (req,res)=>{
         res.status(201).json(newTask)
     }catch(error){
         res.status(500).json({error:"No se pudo crear la tarea"})
+    }
+}
+
+const deleteTask= async (req,res)=>{
+    const id=req.params
+    const userId=req.user.userId
+
+    try{
+        //deleteMany puede buscar por varios filtros y delete solo por id 
+        const deletedTask= await prisma.task.deleteMany({where:{id:id,userId:userId}})
+
+
+        if(deletedTask.count ===0){
+            return res.status(404).json({error:"Tarea no encontrada o no tienes permiso para borrarla"})
+        }
+
+        res.json({message:"Tarea eliminada correctamente"})
+    }catch(error){
+
+        res.status(500).json({error:"Error al intentar eliminar tarea"})
+    }
+
+
+}
+
+const updateTask= async (req,res)=>{
+    const id=req.params
+    const {title,description,status,priority, deadline}=req.body
+    const userId=req.user.userId
+
+    try{
+        const updatedTask= await prisma.task.updateMany({ where:{id:id,userId:userId}
+            ,data:{
+                title,
+                description,
+                status,
+                priority,
+                deadline
+            }})
+
+
+            if(updatedTask.count === 0){
+                return res.status(404).json({error:"Tarea no encontrada o no tienes permiso para modificarla"})
+            }
+            res.json({message:"Tarea actualizada correctamente"})
+    }catch(error){
+        return res.status(500).json({error:"Error al actualizar tarea"})
+    }
+
+    
+}
+
+
+const getTaskbyId =async(req,res)=>{
+
+    const id = req.params.id
+    const userId=req.user.userId
+
+    try{
+        const task= await prisma.task.findFirst({
+            where:{
+                id:id,
+                userId:userId
+            }
+        })
+        if (!task) return res.status(404).json({ error: "Tarea no encontrada" });
+
+        res.json(task);
+    } catch (error) {
+        res.status(500).json({ error: "Error al obtener la tarea" });
     }
 }
 
