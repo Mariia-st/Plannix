d9fa7c23729bd9cfca4b1d955e8cc3aeb79f0dcd
Marria <maskastarik@gmail.com>
2026-06-12 18:45:11 +0200

    feat: Añadir rutas nuevas

--- plannix-back-end/src/routes/taskRoutes.js
+++ plannix-back-end/src/routes/taskRoutes.js
@@ -1,11 +1,14 @@
 const express=require('express')
 const router=express.Router()
-const {getTasks,createTasks}= require('../taskController')
+const {getTasks,createTasks,deleteTask,updateTask,getTaskbyId}= require('../taskController')
 const authenticateToken=require('../middlewares/authMiddleware')
 
 
 router.get('/',authenticateToken,getTasks)
 router.post('/',authenticateToken,createTasks)
+router.delete('/:id',authenticateToken,deleteTask)
+router.post('/:id',authenticateToken,updateTask)
+router.get('/:id',authenticateToken,getTaskbyId)
 
 
 module.exports=router;
