030a3086c84798fcd6cfd8ff7607775aded260f8
Marria <maskastarik@gmail.com>
2026-06-16 17:28:42 +0200

    feat: Añadir nueva ruta de home

--- plannix-front-end/src/App.jsx
+++ plannix-front-end/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
+import Login from "./components/login";
 import Layout from "./components/Layout";
+import Home from "./components/Home";
 
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <Layout>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="home" element={<Home />} />
       </Routes>
     </Layout>
   );
