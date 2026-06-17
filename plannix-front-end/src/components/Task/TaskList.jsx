f5a3b78a210262c3fbf0c1ae96bfe279316bbe6c
Marria <maskastarik@gmail.com>
2026-06-17 18:38:09 +0200

    update: taskList
    -control de boton con loading

--- plannix-front-end/src/components/Task/TaskList.jsx
+++ plannix-front-end/src/components/Task/TaskList.jsx
@@ -1,4 +1,4 @@
-export default function TaskList({ tasks, onSelectTask, priorityColors }) {
+export default function TaskList({ tasks, onSelectTask, priorityColors, loading }) {
     return (
       <div className="card border-0 shadow-sm p-4 h-100">
         <h4 className="mb-4">Mis tareas</h4>
@@ -12,8 +12,8 @@ export default function TaskList({ tasks, onSelectTask, priorityColors }) {
               <span className={`badge ${priorityColors[task.priority] || "bg-light"} mx-2`}>
                 {task.priority}
               </span>
-              <span className="text-muted small">{task.status}</span>
-              <button 
+              <span className="text-muted small  px-2">{task.status}</span>
+              <button  disabled={loading}
                 className="btn btn-sm btn-outline-primary" 
                 onClick={() => onSelectTask(task.id)}
               >
