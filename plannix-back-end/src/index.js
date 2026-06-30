e3b134db99c865e24d2b019ae9d7bacdd9939c16
Marria <maskastarik@gmail.com>
2026-06-30 18:40:49 +0200

    feat: index, bot
    -añadimos el bot y su funcionamiento para poder manejar la app desde el telegram
    - en index lanzamos el bot
    -codigo limpio y comentado

--- plannix-back-end/src/index.js
+++ plannix-back-end/src/index.js
@@ -1,15 +1,26 @@
+//servidor,cors
 const express = require("express");
 const cors = require("cors")
-const authRoutes = require('./routes/authRoutes')
-const taskRoutes= require('./routes/taskRoutes')
-const userRoutes= require("./routes/userRoute")
-const path = require("path")
-//carga datos de .env
-require('dotenv').config();
+
 //uso de servidor
 const app = express();
 //port
 const PORT = 3000;
+
+//ficheros de rutas
+const authRoutes = require('./routes/authRoutes')
+const taskRoutes= require('./routes/taskRoutes')
+const userRoutes= require("./routes/userRoute")
+//path para carpeta uploads
+const path = require("path")
+
+//bot
+const bot= require("./bot_telegram/bot")
+
+
+
+//carga datos de .env
+require('dotenv').config();
 
 //  CORS para todas las peticiones 
 app.use(cors({
@@ -21,7 +32,7 @@ app.use(cors({
 //esto hace que la carpeta sea accesible desde el navegador
 app.use("/uploads", express.static(path.join(__dirname,"public","uploads")))
 
-//Conectamos el middleware para leer jgon
+//Conectamos el middleware para leer json
 app.use(express.json())
 
 app.get('/',(req,res)=>{
@@ -35,6 +46,16 @@ app.use('/auth',authRoutes)
 app.use('/tasks',taskRoutes)
 app.use("/user",userRoutes)
 
+
+
+
+
+//lanzamos el bot 
+bot.launch().then(() => {
+    console.log("PlannixBot está en línea!");
+}).catch((err) => {
+    console.error("Error al iniciar el bot:", err);
+});
 
 app.listen(PORT,()=>{
     console.log(`Servidor funciona en http://localhost:${PORT}`)
