b9c3f72842f59c054320ce68092f0694f0087731
Marria <maskastarik@gmail.com>
2026-07-07 17:04:32 +0200

    fix:el bot se lanza despues de servidor

--- plannix-back-end/src/index.js
+++ plannix-back-end/src/index.js
@@ -23,6 +23,69 @@ const bot= require("./bot_telegram/bot")
 require('dotenv').config();
 
 //  CORS para todas las peticiones 
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || 
+            origin.includes('localhost') || 
+            origin.endsWith('.vercel.app')) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
+//esto hace que la carpeta sea accesible desde el navegador
+app.use("/uploads", express.static(path.join(__dirname,"public","uploads")))
+
+//Conectamos el middleware para leer json
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('Servidor funciona')
+})
+
+//Conectamos las rutas de autorización 
+//Cualquier petición que empiece con /auth Centralízala y mándala al archivo authRoutes
+app.use('/auth',authRoutes)
+
+app.use('/tasks',taskRoutes)
+app.use("/user",userRoutes)
+
+
+
+
+
+∑
+app.listen(PORT,'0.0.0.0',()=>{
+    console.log(`Servidor funciona en http://localhost:${PORT}`)
+    //servidor,cors
+const express = require("express");
+const cors = require("cors")
+
+//uso de servidor
+const app = express();
+//port
+const PORT = process.env.PORT || 3000;
+
+//ficheros de rutas
+const authRoutes = require('./routes/authRoutes')
+const taskRoutes= require('./routes/taskRoutes')
+const userRoutes= require("./routes/userRoute")
+//path para carpeta uploads
+const path = require("path")
+
+//bot
+const bot= require("./bot_telegram/bot")
+
+
+
+//carga datos de .env
+require('dotenv').config();
+
+//  CORS para todas las peticiones 
 app.use((req, res, next) => {
     const origin = req.headers.origin;
     // Разрешаем любой запрос с твоего фронтенда или localhost
@@ -70,4 +133,5 @@ bot.launch().then(() => {
 
 app.listen(PORT,'0.0.0.0',()=>{
     console.log(`Servidor funciona en http://localhost:${PORT}`)
+})
 })
