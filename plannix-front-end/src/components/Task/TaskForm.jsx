410fe3514120e34d1a1ceb55436ae9675592d183
Marria <maskastarik@gmail.com>
2026-07-07 11:26:47 +0200

    feat:taskForm cambios de diseño

--- plannix-front-end/src/components/Task/TaskForm.jsx
+++ plannix-front-end/src/components/Task/TaskForm.jsx
@@ -1,60 +1,80 @@
 // components/TaskForm.js
-export default function TaskForm({ formData, setFormData, onSubmit, error,loading }) {
-  
+export default function TaskForm({
+  formData,
+  setFormData,
+  onSubmit,
+  error,
+  loading,
+}) {
   // guardamos datos de formulario
-    const handleChange = (e) => {
-      const { name, value } = e.target;
-      setFormData((prev) => ({ ...prev, [name]: value }));
-    };
- 
-  
-    return (
-      <div className="card border-0 shadow-sm p-4 h-100" >
-        <h4 className="mb-4">Crear nueva tarea</h4>
-        <form className="d-flex flex-column gap-3" onSubmit={onSubmit} >
-          <input 
-            name="title" 
-            placeholder="Título" 
-            className="form-control" 
-            onChange={handleChange} 
-            value={ formData.title} 
-          
-          />
-          <div className="text-danger small ">{error}</div>
-          <textarea 
-            name="description" 
-            placeholder="Descripción" 
-            className="form-control" 
-            rows="3" 
-            onChange={handleChange} 
-            value={formData.description}
-          />
-          <div className="row g-2">
-            <div className="col">
-              <select name="status" className="form-select" onChange={handleChange} value={formData.status}>
-                <option value="pediente">Pendiente</option>
-                <option value="en curso">En curso</option>
-                <option value="completada">Completada</option>
-              </select>
-            </div>
-            <div className="col">
-              <select name="priority" className="form-select" onChange={handleChange} value={formData.priority}>
-                <option value="baja">Baja</option>
-                <option value="media">Media</option>
-                <option value="alta">Alta</option>
-                <option value="urgente">Urgente</option>
-              </select>
-            </div>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <div className="card border-0 shadow-sm p-4 h-100">
+      <h4 className="mb-4">Crear nueva tarea</h4>
+      <form className="d-flex flex-column gap-3" onSubmit={onSubmit}>
+        <input
+          name="title"
+          placeholder="Título"
+          className="form-control"
+          onChange={handleChange}
+          value={formData.title}
+        />
+        <div className="text-danger small ">{error}</div>
+        <textarea
+          name="description"
+          placeholder="Descripción"
+          className="form-control"
+          rows="3"
+          onChange={handleChange}
+          value={formData.description}
+        />
+        <div className="row g-2">
+          <div className="col">
+            <select
+              name="status"
+              className="form-select"
+              onChange={handleChange}
+              value={formData.status}
+            >
+              <option value="pediente">Pendiente</option>
+              <option value="en curso">En curso</option>
+              <option value="completada">Completada</option>
+            </select>
           </div>
-          <input 
-            name="deadline" 
-            type="date"
-            className="form-control" 
-            onChange={handleChange} 
-            value={formData.deadline} 
-          />
-          <button type="submit" className="btn btn-primary w-100"  disabled={loading}>Guardar Tarea</button>
-        </form>
-      </div>
-    );
-  }
+          <div className="col">
+            <select
+              name="priority"
+              className="form-select"
+              onChange={handleChange}
+              value={formData.priority}
+            >
+              <option value="baja">Baja</option>
+              <option value="media">Media</option>
+              <option value="alta">Alta</option>
+              <option value="urgente">Urgente</option>
+            </select>
+          </div>
+        </div>
+        <input
+          name="deadline"
+          type="date"
+          className="form-control"
+          onChange={handleChange}
+          value={formData.deadline}
+        />
+        <button
+          type="submit"
+          className="btn btn-primary w-100"
+          disabled={loading}
+        >
+          Guardar Tarea
+        </button>
+      </form>
+ 
+    </div>
+  );
+}
