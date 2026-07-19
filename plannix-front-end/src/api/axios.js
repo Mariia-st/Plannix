7442906ea697cdb773bd6ae499099fc4b9a67eaa
Marria <maskastarik@gmail.com>
2026-07-19 15:27:12 +0200

    feat: llamamos al refresh

--- plannix-front-end/src/api/axios.js
+++ plannix-front-end/src/api/axios.js
@@ -1,24 +1,65 @@
 import axios from 'axios';
 
 //configuracion base de mi api
-const api=axios.create({
+const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL, //mi back
-    headers:{
-        'Content-Type':'application/json'
+    headers: {
+        'Content-Type': 'application/json'
     },
 });
 
 //se ejecuta antes de cada pet
-api.interceptors.request.use((config)=>{
-    const token= localStorage.getItem('token');
-    if(token){
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
         // si existe añadimos header
-        config.headers.Authorization=`Bearer ${token}`
+        config.headers.Authorization = `Bearer ${token}`
     }
 
     return config
-},(error)=>{
+}, (error) => {
     return Promise.reject(error)
 })
+
+
+// se ejecuta cuando recibimos la respuesta de servidor 
+api.interceptors.response.use((response) => {
+    response
+}, // si todo bien dejo pasar 
+    async (error) => { //si error pasamos 
+        const origRequest = error.config // lo guardamos para repetir la peticíon 
+
+        // verificamos error y _retry 
+        if (error.response?.status === 401 && !origRequest._retry) {
+            // ponemos a true para no entrar a buvle infinito
+            origRequest._retry = true
+
+
+            try {
+                // const {data} igual y entra a data directamente
+                //hacemos refresh
+                const res = await api.post("/refresh")
+                // guardamos
+                localStorage.setItem("token", res.data.access_token)
+                //ponemos el header de nuevo 
+                origRequest.headers.Authorization = `Bearer ${res.data.access_token}`
+                //hacemos la petición otra vez
+                return api(origRequest)
+
+            } catch (refreshError) {
+                // Si el refresh también falla, el usuario debe volver a loguearse
+                localStorage.removeItem('token');
+                window.location.href = '/';
+                return Promise.reject(refreshError);
+            }
+
+
+        }
+
+        //error
+        return Promise.reject(error);
+
+    }
+)
 
 export default api
